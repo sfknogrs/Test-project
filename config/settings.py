e923b653a0d6059126092e881f4b882904865ff6
sfknogrs <safakaanogras@gmail.com>
2023-04-16 22:13:43 +0000

    test moby

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'betterpwd'
+
+APP_NAME = 'Moby Dock'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://mobydock:kaanpwd@postgres:5432/mobydock'
+REDIS_URL = 'redis://redis:6379/0'
+
